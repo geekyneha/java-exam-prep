e2bd95d69744b39ce3e387ee6cf0661ca9b9e26b
geekyneha <nehatualea@gmail.com>
2026-01-17 14:25:03 +0530

    Thread creation by extending the Thread class

--- module3/threads/MyThread.java
+++ module3/threads/MyThread.java
@@ -20,8 +20,9 @@ public class MyThread extends Thread {
      
     public void run(){
         for(int i=0; i<10; i++){
-            System.out.println("Thread is running: " + i);
+            System.out.println(Thread.currentThread().getName());
         }
+        
     }
     public static void main(String[] args) {
 
@@ -29,7 +30,7 @@ public class MyThread extends Thread {
         t1.start();
 
         for(int i=0; i<10; i++){
-            System.out.println("Main thread: " + i);
+            System.out.println(Thread.currentThread().getName());
         }
 
 
