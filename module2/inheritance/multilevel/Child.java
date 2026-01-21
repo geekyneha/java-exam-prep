46f57cd1cd098611e197f9af31a46bcf12c45b03
geekyneha <nehatualea@gmail.com>
2026-01-21 19:47:40 +0530

    Inheritance: Multilevel inheritance

--- module2/inheritance/multilevel/Child.java
+++ module2/inheritance/multilevel/Child.java
@@ -9,9 +9,13 @@ public class Child extends Parent{
 
     public static void main(String[] args){
         Child child = new Child("Harry");
+        Child child2 = new Child("Ginny");
         System.out.println("Child Name: " + child.name);
         System.out.println("Child SurName: " + child.surName);
         child.displayInfo();
+        System.out.println("Child Name: " + child2.name);
+        System.out.println("Child SurName: " + child2.surName);
+        child2.displayInfo();
     }
     
 }
