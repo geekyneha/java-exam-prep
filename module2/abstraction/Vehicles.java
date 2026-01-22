d692bd866cb768e8f1e49695b75bf4b0370ee8bc
geekyneha <nehatualea@gmail.com>
2026-01-22 19:59:46 +0530

    Abstraction

--- module2/abstraction/Vehicles.java
+++ module2/abstraction/Vehicles.java
@@ -4,8 +4,12 @@ package module2.abstraction;
 // Abstract methods does not have a body and must be implemented by subclasses. abstract methods are declared using the abstract keyword. abstract methods can only exist in abstract classes. abstract classes can have both abstract and concrete methods. 
 
 public abstract class Vehicles {
-
-    public abstract void accelerate();
+        // Contructor of abstract class must be protected because they are only called when a subclass is instantiated. so there is no point in having a public constructor for an abstract class. private constructor is not allowed in abstract class because it prevents subclass from calling the constructor of the abstract class.
+        protected Vehicles(){
+        System.out.println("Vehicle is created");
+    }
+    //Abstract methods are meant to be overriden in subclasses. public access modifier allows the methods to be accessed by any a non-subclass which is not ideal for abstract class methods. protected access modifier allows the methods to be accessed only within the same package or by subclasses which is more appropriate for abstract class methods.
+    protected abstract void accelerate();
     public abstract void brake();
     public abstract void honk();
     public abstract void refuel();
