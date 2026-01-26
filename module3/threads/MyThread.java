32a5af2c7ba377381a6977d00da2fd01b34047b5
geekyneha <nehatualea@gmail.com>
2026-01-26 21:33:43 +0530

    sychronized Thread

--- module3/threads/MyThread.java
+++ module3/threads/MyThread.java
@@ -27,7 +27,9 @@ public class MyThread extends Thread {
     public static void main(String[] args) {
 
         MyThread t1= new MyThread();
+        MyThread t2 = new MyThread();
         t1.start();
+        t2.start();
 
         for(int i=0; i<10; i++){
             System.out.println(Thread.currentThread().getName());
