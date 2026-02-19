8d785857b4953e46b9650b6ea037ba9ea6cd72f0
geekyneha <nehatualea@gmail.com>
2026-02-19 20:33:51 +0530

    Array list

--- module1/cf/ArrayList.java
+++ module1/cf/ArrayList.java
@@ -8,6 +8,13 @@ int a;
 public static void main(String args []){
     int arr[] = {1,23,4,6};
     System.out.println(arr+" "+arr[2]);
+    int arr1[] = new int[5];
+    arr1[0] = 1;
+    arr1[1] = 2;
+    arr1[2] = 3;
+    arr1[3] = 4;        
+    arr1[4] = 5;
+    System.out.println(arr1+" "+arr1[2]);
 }
 
     
