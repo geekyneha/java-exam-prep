b39283754f97e6ca1ed7ed4730f05b997ef93134
geekyneha <nehatualea@gmail.com>
2026-03-07 22:56:20 +0530

    linked list

--- module1/cf/LinkedList/LinkedList.java
+++ module1/cf/LinkedList/LinkedList.java
@@ -40,7 +40,7 @@ public class LinkedList {
             LinkedList list = new LinkedList();
             list.insert(10);
             list.insert(20);
-            list.insert(40);
+            list.insert(80);
             list.display(); // Output: 10 20 30
         }
     
